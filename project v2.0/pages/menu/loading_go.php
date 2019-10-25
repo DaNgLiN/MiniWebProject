5c92fd076c35e65834469eedd82549fb588f0e5f
aashish054 <aashishrana5@gmail.com>
2019-10-25 20:46:33 +0530

    few more bug added

---
+++ project v2.0/pages/menu/loading_go.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" href="css/loader_wall.css">
+<div class="container-fluid bg-danger position-absolute" id="_loader_wall"
+        style="width:100vw;height:100vh;z-index:1000">
+        <div class="d-flex flex-column h-100 w-100 justify-content-center align-items-center ">
+            <div>
+
+                <div class="cssload-box-loading">
+
+                </div>
+            </div>
+            <div class="pt-4">
+                <h1 class="ml1">
+                    <span class="text-wrapper">
+                        <span class="letters">Loading . . .</span>
+                        <span class="line line2"></span>
+                    </span>
+                </h1>
+            </div>
+        </div>
+    </div>
---
+++ project v2.0/scripts/side_menu.js
@@ -0,0 +1,46 @@
+var side_menu_social_icons = document.getElementById("side_menu_social_icons")
+side_menu_social_icons.classList.add("side_menu_social_icons_row");
+
+function sideMenuMouseOver() {
+    var side_menu_social_icons = document.getElementById("side_menu_social_icons")
+    side_menu_social_icons.classList.remove("side_menu_social_icons_row");
+    side_menu_social_icons.classList.add("side_menu_social_icons_col");
+
+    document.getElementById("side_menu_developers").style.display = "none";
+    setTimeout(function() {
+        document.getElementById("side_menu_developers").style.transition = "0.5s";
+        document.getElementById("side_menu_developers").style.opacity = 0;
+    }, 100)
+    document.getElementById("sideMenuLeft").style.display = "none";
+    document.getElementById("sideMenuRight").style.display = "block";
+    document.getElementsByClassName("side_menu")[0].style.width = "5.5vw";
+    document.getElementsByClassName("side_menu")[0].style.transition = "0.3s";
+    var x = document.querySelectorAll(".navbaritems .nav-link div");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.transition = "0.1s";
+        x[i].style.opacity = 0;
+    }
+}
+
+function sideMenuMouseOut() {
+    var side_menu_social_icons = document.getElementById("side_menu_social_icons")
+    side_menu_social_icons.classList.remove("side_menu_social_icons_col");
+    side_menu_social_icons.classList.add("side_menu_social_icons_row");
+
+
+    document.getElementById("side_menu_developers").style.display = "block";
+    setTimeout(function() {
+        document.getElementById("side_menu_developers").style.transition = "1s";
+        document.getElementById("side_menu_developers").style.opacity = 1;
+    }, 100)
+    document.getElementById("sideMenuLeft").style.display = "block";
+    document.getElementById("sideMenuRight").style.display = "none";
+    document.getElementsByClassName("side_menu")[0].style.width = "15vw";
+    document.getElementsByClassName("side_menu")[0].style.transition = "0.3s";
+    var x = document.querySelectorAll(".navbaritems .nav-link div");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.transition = "0.3s";
+        x[i].style.opacity = 1;
+    }
+
+}
